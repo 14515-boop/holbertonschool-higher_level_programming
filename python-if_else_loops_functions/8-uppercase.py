480619d3427b074c704a484f3b7b5c4d4ff9c45f
Ismayil <14515@holbertonstudents.com>
2026-05-30 17:08:08 +0400

    trtr

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    """print a string in uppercase using ASCII conversion only."""
+    result = ""
+    for ch in text:
+        if 'a' <= ch <= 'z':
+            result += chr(ord(ch) - 32)
+        else:
+            result += ch
+    print("{}".format(result))
