712524c52f5535246a6d7f3cefe7eed4ea3d9412
Ismayil <14515@holbertonstudents.com>
2026-05-30 17:31:20 +0400

    gffgfg

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Print the ASCII alphabet in reverse, alternating lower/upper."""
+for i in range(25, -1, -1):
+    c = 122 - i
+    if i % 2 == 0:
+        print("{:c}".format(c), end="")
+    else:
+        print("{:c}".format(c - 32), end="")
