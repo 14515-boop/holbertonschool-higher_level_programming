f7390a9aeb58e1650733152ed8d07c1ec5c8fc8a
Neoda-962 <rcd2007pc@gmail.com>
2026-06-02 19:48:41 +0400

    fgfgg

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a + b
-print("{} + {} = {}".format(a , b, add(a, b)))
+from ooo import add
+
+if __name__ != "__main__":
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a , b, add(a, b)))
