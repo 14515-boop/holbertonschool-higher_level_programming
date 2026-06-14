ebd9d8948f21fac224d1bce8d6179190c6b8cf4d
14515-boop <14514@holbertonstudents.com>
2026-06-14 23:31:19 +0400

    dfdf

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-
     for i in range(x):
         try:
-            item = my_list[i]
-            print("{:d}".format(item), end="")
+            print("{:d}".format(my_list[i]), end="")
             count += 1
-        except (IndexError, ValueError, TypeError):
-            continue
-
-    print()
+        except (ValueError, TypeError):
+            pass
+    print("")
     return count
