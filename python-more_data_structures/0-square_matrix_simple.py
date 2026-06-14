343cd57362a8d7deacb2aff0adc71c6516503b91
14515-boop <14514@holbertonstudents.com>
2026-06-14 13:53:54 +0400

    fdfd

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-matrix=[[1, 2 ,3], [4, 5, 6], [7, 8, 9]]
+matrix = [[1, 2 ,3], [4, 5, 6], [7, 8, 9]]
+new_matrix = []
+for i in matrix:
+    m = []
+    for j in i:
+        m.append(j**2)
+    new_matrix.append(m)
+print(new_matrix)
