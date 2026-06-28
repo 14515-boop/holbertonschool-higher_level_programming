60d93528ff6eaaf48500460260b8a04c2cb37d6a
14515-boop <14514@holbertonstudents.com>
2026-06-28 18:06:30 +0400

    hb

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""hghjgjgjhg"""
+"""Defines BaseGeometry."""
 
 
-class BaseGeometry():
-    """ihfgjhfgh"""
+class BaseGeometry:
+    """BaseGeometry class."""
+
     def area(self):
-        return
+        """Raises an exception."""
+        raise Exception("area() is not implemented")
