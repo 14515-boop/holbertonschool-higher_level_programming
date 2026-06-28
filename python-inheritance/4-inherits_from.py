76383bc8a7687a90fd6eae0648e47aee06b72e0f
14515-boop <14514@holbertonstudents.com>
2026-06-28 17:51:45 +0400

    cbv

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module that contains the inherits_from function."""
+
+
 def inherits_from(obj, a_class):
-    """Returns True if obj is an instance of a subclass of a_class."""
+    """Return True if obj is an instance of a subclass of a_class."""
     return isinstance(obj, a_class) and type(obj) is not a_class
